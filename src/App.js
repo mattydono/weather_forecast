eddd0d198a7aa1e85ea275336d33a10d8efbf84e
Matthew <matthew.donovan@mac.com>
2019-02-26 17:24:02 +0000

    Fetch: fetch and store weather data

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import cities from './cities.json'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        selectedCity: '',
+        subscribedCities: {}
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <input placeholder="city,country code" onChange={this.onCityChange}/>
+                <button onClick={this.onCitySubscribe}>Subscribe</button>
+            </div>
+        );
+    }
+
+    onCityChange = event => {
+        this.setState({selectedCity: event.currentTarget.value})
+    };
+
+    validateCity = (cities) => {
+        for(let i=0; i < cities.length; i++) {
+            if(this.state.selectedCity === cities[i].name) {
+                return cities[i].name + ',' + cities[i].country;
+            }
+        }
+    };
+
+    onCitySubscribe = () => {
+        const citySubscribe = this.validateCity(cities);
+        fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + citySubscribe + '&APPID=9447e31ed925167b44b4b37981042924')
+            .then( response => response.json())
+            .then(data => this.setState(state => ({
+                subscribedCities: {
+                    ...state.subscribedCities,
+                    [data.city.name]: data
+                }
+            })), console.log(this.state.subscribedCities))
+    };
 }
 
 export default App;
+
+// ,\n\s+"coord": \{\n.+\n.+\n.+\}
+// ^\s+"id":.+,\n
+
+// const ws = new WebSocket('wss://api.coinfloor.co.uk/');  ws.onmessage = message => {   console.log(message); }
