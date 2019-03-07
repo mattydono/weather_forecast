58dd5e451f182291c9af5313e11f3efa94bcbc90
Kyle Smith <sharakai.software@gmail.com>
2019-03-07 21:41:44 +0000

    chore: format existing code

--- src/DataHandling.js
+++ src/DataHandling.js
@@ -1,32 +1,29 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 export class DataHandling extends Component {
-    render() {
-        const { subscribedCities } = this.props;
-        return (
-            <div>
-                {Object.values(subscribedCities).map(this.renderCityData)}
-            </div>
-        )
-    }
+  render() {
+    const { subscribedCities } = this.props;
+    return (
+      <div>{Object.values(subscribedCities).map(this.renderCityData)}</div>
+    );
+  }
 
-    renderCityData = data => {
-        const cityName = data.city.name;
-        return (
-            <div key={cityName}>
-                <button onClick={() => this.props.unsubscribe(cityName)}>❌</button>
-                    {data.list.map(datum => (
-                    <div>
-                        <div>{cityName}</div>
-                        <div>Date: {datum.dt_txt}</div>
-                        <div>Temp: {(datum.main.temp - 273.15).toFixed(1)}ºC</div>
-                        <div>Humidity: {datum.main.humidity}%</div>
-                        <div>{datum.weather[0].description.toLocaleUpperCase()}</div>
-                        <div>Wind Speed: {(datum.wind.speed * 2.236936).toFixed(1)}mph</div>
-                    </div>
-                    )
-                  )}
-            </div>
-        )
-    };
+  renderCityData = data => {
+    const cityName = data.city.name;
+    return (
+      <div key={cityName}>
+        <button onClick={() => this.props.unsubscribe(cityName)}>❌</button>
+        {data.list.map(datum => (
+          <div>
+            <div>{cityName}</div>
+            <div>Date: {datum.dt_txt}</div>
+            <div>Temp: {(datum.main.temp - 273.15).toFixed(1)}ºC</div>
+            <div>Humidity: {datum.main.humidity}%</div>
+            <div>{datum.weather[0].description.toLocaleUpperCase()}</div>
+            <div>Wind Speed: {(datum.wind.speed * 2.236936).toFixed(1)}mph</div>
+          </div>
+        ))}
+      </div>
+    );
+  };
 }
